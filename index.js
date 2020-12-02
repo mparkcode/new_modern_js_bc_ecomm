d62793d53d0e26198f6a5733638bf3c353b80371
Michael Park <mparkcode@gmail.com>
2020-12-02 21:27:52 +0900

    update to 429

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
-const productsRouter = require('./routes/admin/products');
+const adminProductsRouter = require('./routes/admin/products');
+const productsRouter = require('./routes/products')
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(cookieSession({
   keys: ['sdsdfkvhwlifndcsi3394dk']
 }));
 app.use(authRouter);
+app.use(adminProductsRouter);
 app.use(productsRouter);
 
 
